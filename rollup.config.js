4cc03f39e5f2353395893e684dac79c1979a1fca
Chris Ranz <git@christianranz.com>
2020-03-27 09:11:57 +0100

    Remove dependencies from external

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ const pkg = require('./package.json');
 
 export default {
   input: 'src/index.ts',
-  external: Object.keys(pkg.dependencies).concat(pkg.peerDependencies),
+  external: Object.keys(pkg.peerDependencies),
   output: [{
     dir: pkg.main,
     format: 'cjs'

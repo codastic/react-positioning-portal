26dd43ae6eadf0ae43dc54ebd554e78e7725b7b0
Mario Volke <mv@codastic.com>
2022-12-06 13:19:26 +0100

    0.6.0

--- lib/components/PositioningPortal.d.ts
+++ lib/components/PositioningPortal.d.ts
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 export interface Props<Strategy> {
     children: React.ReactNode;
     portalElement?: React.ReactElement;
--- lib/legacy/index.js/components/PositioningPortalWithState.stories.d.ts
+++
@@ -1,6 +0,0 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { Position } from './PositioningPortal';
-import PositioningPortalWithState from './PositioningPortalWithState';
-declare const _default: ComponentMeta<typeof PositioningPortalWithState>;
-export default _default;
-export declare const Base: ComponentStory<PositioningPortalWithState<Position>>;
